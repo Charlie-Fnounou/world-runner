04a03388fcba19978561e03384ac475516cbb275
World Runner <fcharlie507@gmail.com>
2026-07-17 11:04:07 -0500

    Cambiar el mapa a un estilo oscuro tipo Strava
    Reemplaza las teselas clásicas de OpenStreetMap por CARTO Dark Matter
    (gratis, sin necesidad de cuenta ni API key), para un look más moderno
    y oscuro parecido al mapa global de Strava.

--- src/components/MapaMundial.tsx
+++ src/components/MapaMundial.tsx
@@ -20,8 +20,10 @@ export function MapaMundial({ carreras, alto = 460 }: { carreras: Carrera[]; alt
         worldCopyJump
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          subdomains="abcd"
+          maxZoom={19}
         />
         {carreras.map((r) => {
           const info = ESTADO_INFO[r.status];
